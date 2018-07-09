c28777dcdd5ff531405169347c93aed89ad2987f
baconadmiral <andrewwilliams1087@gmail.com>
2018-07-09 16:29:27 -0600

    correct flow
    removed bad flow added good flow

---
+++ js/module3.js
@@ -0,0 +1,8 @@
+//@flow
+
+export function foo(x: ?number): string {
+  if (x) {
+    return x;
+  }
+  return "default string";
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
+//@flow
 console.log('Hello World!');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
+import {foo} from './module3';
+
+foo("something");
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
